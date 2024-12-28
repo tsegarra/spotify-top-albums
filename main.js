a82528c67909d3c3102b16315773eee9b233e942
tomsegarra <tomsegarra@gmail.com>
2024-12-28 17:23:46 -0500

    Add more logging

--- main.js
+++ main.js
@@ -14100,7 +14100,7 @@
       }
     };
     const body = await fetch("https://api.spotify.com/v1/tracks/" + trackId, payload);
-    alert(body.text());
+    body.text().then((s) => alert(s)).catch((e2) => alert(e2));
     const response = await body.json();
     const imageUrl = response.album.images[0].url;
     cacheImageUrlForTrack(trackId, imageUrl);
